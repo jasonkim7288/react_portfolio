a86170453d4b033c4b4c6b0875ae895954d3399f
jasonkim7288 <jason.kim7288@gmail.com>
2020-10-26 00:00:03 +1000

    deploy link error bug fixed - 3

--- src/components/BurgerMenu.js
+++ src/components/BurgerMenu.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { slide as Menu } from 'react-burger-menu';
 
 function BurgerMenu() {
   return (
     <div id="burger-display">
     <Menu right width={ '280px' } disableAutoFocus>
-      <a href="/" id="home" className="menu-item">Home</a>
-      <a href="/about" id="about" className="menu-item">About</a>
-      <a href="/projects" id="projects" className="menu-item">Projects</a>
-      <a href="/contact" id="contact" className="menu-item">Contact</a>
+      <Link to="/" id="home" className="menu-item">Home</Link>
+      <Link to="/about" id="about" className="menu-item">About</Link>
+      <Link to="/projects" id="projects" className="menu-item">Projects</Link>
+      <Link to="/contact" id="contact" className="menu-item">Contact</Link>
     </Menu>
     </div>
   )
